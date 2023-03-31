a946adf33b6998dd38f1dca0e35f89ea3cfb101f
Edwin <edwinjoye@gmail.com>
2023-03-31 11:38:58 +0200

    images changees

--- src/data/animations.js
+++ src/data/animations.js
@@ -4,7 +4,7 @@ export const animations = [
     title: "Vidéo de squate 1",
     categorie: "1",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1678896718/carnet2plans/VIDEOS/ipados13-ipad-pro-photos-video-edit-slow-motion_dtwffv.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187022/carnet2plans/Work/Animations/ipados13-ipad-pro-photos-video-edit-filters_q7n8ac.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -13,7 +13,7 @@ export const animations = [
     title: "Montage vidéo 1",
     categorie: "2",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1678896718/carnet2plans/VIDEOS/Hero__4__zean2g.png",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187022/carnet2plans/Work/Animations/ipados13-ipad-pro-photos-video-edit-slow-motion_ii1tol.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -22,7 +22,7 @@ export const animations = [
     title: "Vidéo de BMX",
     categorie: "1",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1678896717/carnet2plans/VIDEOS/ipados13-ipad-pro-photos-video-edit-trim_zjruo6.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187021/carnet2plans/Work/Animations/Hero__4__ekf2s4.png",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -31,7 +31,7 @@ export const animations = [
     title: "Vidéo de squate 2",
     categorie: "2",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1678896716/carnet2plans/VIDEOS/ipados13-ipad-pro-photos-video-edit-filters_v8aprt.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187021/carnet2plans/Work/Animations/Hero__4__ekf2s4.png",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -40,7 +40,7 @@ export const animations = [
     title: "Montage vidéo 2",
     categorie: "1",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1678896715/carnet2plans/VIDEOS/imovie-app_yyzx0i.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187021/carnet2plans/Work/Animations/ipados13-ipad-pro-photos-video-edit-trim_pp5wwd.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
--- src/data/realisations.js
+++ src/data/realisations.js
@@ -13,7 +13,7 @@ export const realisations = [
     title: "Saint-Germain",
     categorie: "2",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187350/carnet2plans/Work/Real/71695084_1372003632956611_9079706278451740672_o_sljbyf.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187348/carnet2plans/Work/Real/70630030_1372004242956550_2679921561045041152_o_nfd1fl.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -22,7 +22,7 @@ export const realisations = [
     title: "Orléans",
     categorie: "1",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187350/carnet2plans/Work/Real/71695084_1372003632956611_9079706278451740672_o_sljbyf.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187339/carnet2plans/Work/Real/70489356_1372009766289331_8872331552160219136_o_toopbu.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -31,7 +31,7 @@ export const realisations = [
     title: "Rouen",
     categorie: "2",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187350/carnet2plans/Work/Real/71695084_1372003632956611_9079706278451740672_o_sljbyf.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187265/carnet2plans/Work/Real/IMG_20200708_232502_aggxob.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
@@ -40,7 +40,7 @@ export const realisations = [
     title: "Toulouse",
     categorie: "1",
     mainUrl:
-      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187350/carnet2plans/Work/Real/71695084_1372003632956611_9079706278451740672_o_sljbyf.jpg",
+      "https://res.cloudinary.com/djfi7lj5z/image/upload/v1680187242/carnet2plans/Work/Real/IMG_20200708_232934_nnlu3k.jpg",
     texte:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.",
   },
